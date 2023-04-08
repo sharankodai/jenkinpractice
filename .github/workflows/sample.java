50fd0353f107f2c9e05ca73df99d30df25d9efae
sharankodai <123050149+sharankodai@users.noreply.github.com>
2023-04-08 20:43:00 +0530

    Update sample.java

--- .github/workflows/sample.java
+++ .github/workflows/sample.java
@@ -2,4 +2,6 @@ public class sample{
 public static void main(String[] args){
 System.out.println("my name is kumar");
 System.out.println("jenkins practice");
+  System.out.println("cheking");
+
 }}
